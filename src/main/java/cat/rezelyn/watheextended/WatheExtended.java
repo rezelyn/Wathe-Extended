4f544cc1ae1023ec1581a314784ba1705ac722f5
Rezelyn <rezelxn@gmail.com>
2026-09-12 06:43:27 +0200

    fix: properly clear poison and infected effects on game stop
    Note: Poison clear should normally already be handled by Wathe correctly, I added another check just to be sure
    
    Fixes #84

--- src/main/java/cat/rezelyn/watheextended/WatheExtended.java
+++ src/main/java/cat/rezelyn/watheextended/WatheExtended.java
@@ -9,7 +9,9 @@ import cat.rezelyn.watheextended.index.*;
 import cat.rezelyn.watheextended.modifiers.WatheExtendedModifiers;
 import cat.rezelyn.watheextended.modifiers.IntrovertedModifier;
 import dev.doctor4t.wathe.cca.GameWorldComponent;
+import dev.doctor4t.wathe.cca.PlayerPoisonComponent;
 import net.fabricmc.api.ModInitializer;
+import org.agmas.noellesroles.infected.InfectedPlayerComponent;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
@@ -43,9 +45,25 @@ public class WatheExtended implements ModInitializer {
             GameWorldComponent gwc = GameWorldComponent.KEY.get(world);
             if (gwc == null) return;
             if (gwc.getGameStatus() == GameWorldComponent.GameStatus.STOPPING) {
+                // clear all potion effects
                 List<StatusEffectInstance> effects = new java.util.ArrayList<>(player.getStatusEffects());
                 for (StatusEffectInstance effect : effects) {
                     player.removeStatusEffect(effect.getEffectType());
+                }
+                // clear poison effect
+                try {
+                    PlayerPoisonComponent poison = PlayerPoisonComponent.KEY.get(player);
+                    if (poison != null) {
+                        poison.poisoner = null;
+                        poison.reset();
+                    }
+                } catch (Throwable ignored) {
+                }
+                // clear infected effect
+                try {
+                    InfectedPlayerComponent infected = InfectedPlayerComponent.KEY.get(player);
+                    if (infected != null) infected.reset();
+                } catch (Throwable ignored) {
                 }
             }
         } catch (Throwable ignored) {
@@ -301,6 +319,12 @@ public class WatheExtended implements ModInitializer {
             if (game == null) return;
 
             GameWorldComponent.GameStatus status = game.getGameStatus();
+
+            if (status == GameWorldComponent.GameStatus.STOPPING) {
+                for (ServerPlayerEntity player : serverWorld.getPlayers()) {
+                    clearEffects(serverWorld, player);
+                }
+            }
 
             TeleportationHandler.tick(serverWorld, status, worldTime);
 
